7c41938ee22bfd5851445314759c59fb8f864abc
Unknownboy627 <otooandrew130@gmail.com>
2025-12-14 12:07:09 +0000

    Set app to run on specified port in production

--- dashboard/app.py
+++ dashboard/app.py
@@ -57,6 +57,7 @@ def dashboard():
 def logout():
     session.clear()
     return redirect("/")
-
-if __name__ == "__main__":
-    app.run() 
+    
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
